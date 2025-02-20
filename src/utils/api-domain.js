6d5b6d0a7ee661ea6593500f5445f6da09b42004
dokhacduong3 <dokhacduong3@gmail.com>
2025-02-20 15:07:05 +0700

    Upload Web V1

--- src/utils/api-domain.js
+++ src/utils/api-domain.js
@@ -1,3 +1,3 @@
 // export const DOMAIN = "35.198.196.205"
 // export const DOMAIN ="project-duong-vip-api.vercel.app"
-export const DOMAIN="http://localhost:2709"
+export const DOMAIN="http://103.118.29.252:2709"
